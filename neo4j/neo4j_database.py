8c105d2e59a8074f32e2dcfa67fc59f0f7b6f242
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-06-15 08:02:07 +0200

    Change name of DB

--- neo4j/neo4j_database.py
+++ neo4j/neo4j_database.py
@@ -1,13 +1,15 @@
 from py2neo.ogm import Repository
 import os
 
+DB_NAME = "ct-service"
+
 class Neo4jDatabase():
   
   def __init__(self):
     url = os.environ['CDISC_CT_LOAD_SERVICE_NEO4J_URL']
     usr = os.environ['CDISC_CT_LOAD_SERVICE_NEO4J_USER']
     pwd = os.environ['CDISC_CT_LOAD_SERVICE_NEO4J_PWD']
-    self.__repo = Repository(url, name="neo4j", user=usr, password=pwd)
+    self.__repo = Repository(url, name=DB_NAME, user=usr, password=pwd)
 
   def repository(self):
     return self.__repo
